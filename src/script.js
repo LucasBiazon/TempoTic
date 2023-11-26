22ae7e35f22afd737e99183224d75d757bad4df3
Lucas Biazon Palma <lucasbiazonpalma@gmail.com>
2023-11-26 19:56:32 -0300

    Index.html

---
+++ src/script.js
@@ -0,0 +1,4 @@
+const dateInitial = document.querySelector("#dateInitial") 
+const result = document.querySelector("#result")
+const inputInitial = document.querySelector("#inputInitial")
+const inputFinal = document.querySelector("#inputFinal")
